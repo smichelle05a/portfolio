f7ef3fdf168e99d0aa40869dfc4949039fd45493
Scarlet <smichelle05a@gmail.com>
2022-01-26 21:55:16 -0300

    example pages to check navigation

---
+++ src/pages/page-cta.jsx
@@ -0,0 +1,11 @@
+import Layout from '../layout/Layout'
+
+function PageCTA() {
+  return (
+    <Layout>
+      <h1>PageCTA</h1>
+    </Layout>
+  );
+}
+
+export default PageCTA;
---
+++ src/pages/page-one.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../layout/Layout';
+
+function PageOne() {
+  return (
+    <Layout>
+      <h1>PageOne</h1>
+    </Layout>
+  );
+}
+
+export default PageOne;
