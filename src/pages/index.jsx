b1d32e9ab1ee755bd4783faaa0408cf4e720d2d2
Scarlet <smichelle05a@gmail.com>
2022-01-29 17:16:08 -0300

    add landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
 import Layout from '../layout/Layout'
+import home from '../styles/home.module.scss'
 
 function Home() {
   return (
     <Layout>
-      <h1>Hola desde Home</h1>
+      <div className={`row p-none ${home.hero}`}>
+        <div className={`col d-flex fd-col ai-center h-100 jc-center ${home.hero__title}`}>
+          <h2 className="color-primary-lighter fs-16">Hi, my name is</h2>
+          <h1 className="color-highlight fs-36"> Scarlet Albornoz</h1>
+          <h2 className="color-primary-lighter fs-16">
+            Welcome to my Portfolio
+          </h2>
+        </div>
+        <div className={`col p-none d-flex`}>
+          <img src='/assets/img/landing-photo.png' alt="Scarlet Albornoz" className={`${home.hero__img} p-none`} />
+        </div>
+      </div>
     </Layout>
   )
 }
