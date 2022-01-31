713461b4830fbaf102cc36ba50d4d8c1870094df
Scarlet <smichelle05a@gmail.com>
2022-01-31 15:02:42 -0300

    add component bg-animation and remove it from layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -19,16 +19,6 @@ function Layout({children}) {
         {children}
       </main>
       <Footer/>
-
-      <div className="light x1"></div>
-      <div className="light x2"></div>
-      <div className="light x3"></div>
-      <div className="light x4"></div>
-      <div className="light x5"></div>
-      <div className="light x6"></div>
-      <div className="light x7"></div>
-      <div className="light x8"></div>
-      <div className="light x9"></div>
     </>
   )
 }
