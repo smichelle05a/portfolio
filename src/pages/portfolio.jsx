d11bdafbbd93e1870621e01881999e8f2ecab7ab
Scarlet <smichelle05a@gmail.com>
2022-01-31 19:22:24 -0300

    add gap between projects

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,13 +1,14 @@
 import Project from '../components/Project';
 import useFetch from '../hooks/useFetch';
 import Layout from '../layout/Layout';
+import repo from '../styles/portfolio.module.scss'
 
 function Portfolio() {
   const repos = useFetch()
   return (
     <Layout>
       <h2 className='fs-36 pb-1 pt-2 txt-center'>Portfolio</h2>
-      <div className={`row jc-center ac-center fw-wrap`}>
+      <div className={`row jc-center ac-center fw-wrap ${repo.projects}`}>
         {repos.map((r) => <Project repo = {r} key= {r.id} />)}
       </div>
     </Layout>
