728781f3f366aa0f0bfe9cd0a0aa636233fc9cb8
Scarlet <smichelle05a@gmail.com>
2022-02-02 22:11:40 -0300

    remove landing photo, and replace some styles and "Welcome to my Portfolio" for "Frontend Developer"

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,18 +7,13 @@ import home from '../styles/home.module.scss'
 function Home() {
   return (
     <Layout>
-      <div className={`row p-none ${home.hero}`}>
-        <div className={`col d-flex fd-col ai-center h-100 jc-center ${home.hero__title}`}>
-          <h2 className="color-primary-lighter fs-16">Hi, my name is</h2>
-          <h1 className="color-highlight fs-36"> Scarlet Albornoz</h1>
-          <h2 className="color-primary-lighter fs-16">
-            Welcome to my Portfolio
+        <div className={`col d-flex fd-col ai-center h-100 jc-center grow-1 ${home.hero__title}`}>
+          <h2 className="color-primary-lighter fs-24">Hi, my name is</h2>
+          <h1 className="color-highlight fs-56 txt-center my-3"> Scarlet Albornoz</h1>
+          <h2 className="color-primary-lighter fs-24">
+            Frontend Developer
           </h2>
         </div>
-        <div className={`col p-none d-flex`}>
-          <img src='/assets/img/landing-photo.png' alt="Scarlet Albornoz" className={`${home.hero__img} p-none`} />
-        </div>
-      </div>
       <BgAnimation/>
     </Layout>
   )
