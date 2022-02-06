a119b9d628939b9d1306e5b80ec21e11591004db
Scarlet <smichelle05a@gmail.com>
2022-02-05 21:22:46 -0300

    add classes

--- src/components/experience/Job.jsx
+++ src/components/experience/Job.jsx
@@ -1,12 +1,16 @@
 import JobDescription from './JobDescription';
+import xp from '../../styles/experience.module.scss'
 
 function Job({job}) {
   return (
     <div>
-      <p>
-        {job.occupation} | {job.from} - {job.to}
-      </p>
-      {job.description.map( (desc, index) => <JobDescription desc={desc} key={index}/>)}
+      <div className='d-flex fd-col jc-center py-2'>
+        <p className='fw-600 pb-1'> {job.occupation.toUpperCase()} </p>
+        <p> {job.from} - {job.to} </p>
+      </div>
+      <div>
+        {job.description.map( (desc, index) => <JobDescription desc={desc} key={index}/>)}
+      </div>
     </div>
   );
 }
