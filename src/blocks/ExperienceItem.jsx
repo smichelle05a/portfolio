ea451ca42f245106d8fd2487ab8abf393e71860f
Scarlet <smichelle05a@gmail.com>
2022-02-05 21:17:41 -0300

    increase padding and transform company name to uppercase

--- src/blocks/ExperienceItem.jsx
+++ src/blocks/ExperienceItem.jsx
@@ -3,9 +3,9 @@ import xp from '../styles/experience.module.scss'
 
 function ExperienceItem({exp}) {
   return (
-    <div className={`d-flex fd-col jc-center p-2 rounded-l relative ${xp.timeline__item__description}`}>
-    <h3>
-      {exp.company}
+    <div className={`d-flex fd-col jc-center p-5 rounded-l relative ${xp.timeline__item__description}`}>
+    <h3 className={`txt-center fs-18 fw-800 border-bottom pb-1`}>
+      {exp.company.toUpperCase()}
     </h3>
     {exp.job.map( (j, index) => <Job key={index} job={j}/>)}
     </div>
