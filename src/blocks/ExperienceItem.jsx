ce05e6fe642d494ab05e65e6dbc1de3e33d06396
Scarlet <smichelle05a@gmail.com>
2022-02-05 21:23:24 -0300

    add my-2 to item

--- src/blocks/ExperienceItem.jsx
+++ src/blocks/ExperienceItem.jsx
@@ -3,7 +3,7 @@ import xp from '../styles/experience.module.scss'
 
 function ExperienceItem({exp}) {
   return (
-    <div className={`d-flex fd-col jc-center p-5 rounded-l relative ${xp.timeline__item__description}`}>
+    <div className={`d-flex fd-col jc-center p-5 rounded-l relative my-2 ${xp.timeline__item__description}`}>
     <h3 className={`txt-center fs-18 fw-800 border-bottom pb-1`}>
       {exp.company.toUpperCase()}
     </h3>
