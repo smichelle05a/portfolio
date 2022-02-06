ac6187bf0d814b995b94338f851dff54bf2e943e
Scarlet <smichelle05a@gmail.com>
2022-02-05 22:13:44 -0300

    add about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import Layout from '../layout/Layout'
+
+function About() {
+  return (
+    <Layout>
+      <h1 className='fs-36 py-3 txt-center'>About me</h1>
+    </Layout>
+  );
+}
+
+export default About;
